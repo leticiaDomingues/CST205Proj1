53918e31e538ac3c22e7b5589785dc26e8ead810
Leticia <ledomingues@csumb.edu>
2016-02-10 15:30:22 -0800

    Grayscale + Evil Red filter

--- Project1.py
+++ Project1.py
@@ -95,9 +95,87 @@ class AverageAction(awt.event.ActionListener):
 #action for the click in the "grayscale" button, which executes the median method and then the grayscale
 class GrayscaleAction(awt.event.ActionListener):
     def actionPerformed(self,event):
-      print "GREY"
+      #runs through the pixels of the picture
+      for y in range(0,height):
+        for x in range(0,width):
+          #gets the pixel in the coordinate (x,y) from the 9 pictures and creates a list with all of them
+          pixels = [getPixel(pics[0],x,y),getPixel(pics[1],x,y), getPixel(pics[2],x,y),getPixel(pics[3],x,y),
+                    getPixel(pics[4],x,y),getPixel(pics[5],x,y), getPixel(pics[6],x,y),getPixel(pics[7],x,y),
+                    getPixel(pics[8],x,y)]
+          
+          #creates a list with the blue values of the pixels in the pixels list
+          bluePixels = [getBlue(pixels[0]),getBlue(pixels[1]), getBlue(pixels[2]),getBlue(pixels[3]),
+                        getBlue(pixels[4]),getBlue(pixels[5]), getBlue(pixels[6]),getBlue(pixels[7]),
+                        getBlue(pixels[8])]
+                        
+          #creates a list with the red values of the pixels in the pixels list
+          redPixels = [getRed(pixels[0]),getRed(pixels[1]), getRed(pixels[2]),getRed(pixels[3]),
+                       getRed(pixels[4]),getRed(pixels[5]), getRed(pixels[6]),getRed(pixels[7]),
+                       getRed(pixels[8])]
 
+          #creates a list with the green values of the pixels in the pixels list
+          greenPixels = [getGreen(pixels[0]),getGreen(pixels[1]), getGreen(pixels[2]),getGreen(pixels[3]),
+                         getGreen(pixels[4]),getGreen(pixels[5]), getGreen(pixels[6]),getGreen(pixels[7]),
+                         getGreen(pixels[8])]
+    
+          #sorts all 3 lists of colors, so we can take the median value
+          bluePixels = sorted(bluePixels)
+          redPixels = sorted(redPixels)
+          greenPixels = sorted(greenPixels)
+          
+          #calculates the new RGB value
+          newValue = (redPixels[4] + greenPixels[4] + bluePixels[4])/3
+   
+          #creates a new empty pixel in the coordinate (x,y) in the final picture
+          pixel = getPixel(finalPic,x,y)
+          #creates a new color for the pixel, using the new RGB value
+          newColor = makeColor(newValue, newValue, newValue)
+          setColor(pixel,newColor)
+      #show the final picture to the user    
+      show(finalPic)
+      repaint(finalPic)
 
+#action for the click in the "Evil Red" button
+class EvilRedAction(awt.event.ActionListener):
+    def actionPerformed(self,event):
+      #runs through the pixels of the picture
+      for y in range(0,height):
+        for x in range(0,width):
+          #gets the pixel in the coordinate (x,y) from the 9 pictures and creates a list with all of them
+          pixels = [getPixel(pics[0],x,y),getPixel(pics[1],x,y), getPixel(pics[2],x,y),getPixel(pics[3],x,y),
+                    getPixel(pics[4],x,y),getPixel(pics[5],x,y), getPixel(pics[6],x,y),getPixel(pics[7],x,y),
+                    getPixel(pics[8],x,y)]
+          
+          #creates a list with the blue values of the pixels in the pixels list
+          bluePixels = [getBlue(pixels[0]),getBlue(pixels[1]), getBlue(pixels[2]),getBlue(pixels[3]),
+                        getBlue(pixels[4]),getBlue(pixels[5]), getBlue(pixels[6]),getBlue(pixels[7]),
+                        getBlue(pixels[8])]
+                        
+          #creates a list with the red values of the pixels in the pixels list
+          redPixels = [getRed(pixels[0]),getRed(pixels[1]), getRed(pixels[2]),getRed(pixels[3]),
+                       getRed(pixels[4]),getRed(pixels[5]), getRed(pixels[6]),getRed(pixels[7]),
+                       getRed(pixels[8])]
+
+          #creates a list with the green values of the pixels in the pixels list
+          greenPixels = [getGreen(pixels[0]),getGreen(pixels[1]), getGreen(pixels[2]),getGreen(pixels[3]),
+                         getGreen(pixels[4]),getGreen(pixels[5]), getGreen(pixels[6]),getGreen(pixels[7]),
+                         getGreen(pixels[8])]
+    
+          #sorts all 3 lists of colors, so we can take the median value
+          bluePixels = sorted(bluePixels)
+          redPixels = sorted(redPixels)
+          greenPixels = sorted(greenPixels)
+
+          
+          #creates a new empty pixel in the coordinate (x,y) in the final picture
+          pixel = getPixel(finalPic,x,y)
+          #creates a new color for the pixel, using the median value for red, green and blue
+          newColor = makeColor(redPixels[8], redPixels[4], redPixels[4])
+          setColor(pixel,newColor)
+      #show the final picture to the user    
+      show(finalPic)
+      repaint(finalPic)
+     
 #creates the main frame
 frame = JFrame("CST205 - Project 1", size = (500, 100))
 
@@ -109,11 +187,14 @@ button2 = JButton('Average Method')
 button2.addActionListener(AverageAction())
 button3 = JButton('Grayscale')
 button3.addActionListener(GrayscaleAction())
+button4 = JButton('Evil Red')
+button4.addActionListener(EvilRedAction())
 
 #adds the buttons to the button panel
 buttonPanel.add(button1)
 buttonPanel.add(button2)
 buttonPanel.add(button3)
+buttonPanel.add(button4)
 
 #add the panel to the main frame
 frame.add(buttonPanel)
